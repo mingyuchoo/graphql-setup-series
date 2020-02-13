d420df02d5e43236a3609557f0e03dd9f1755a8c
mingyuchoo <mgch@lgcns.com>
2020-02-13 09:10:37 +0900

    modified

---
+++ 04-graphql-yoga/README.md
@@ -0,0 +1,20 @@
+# 04-graphql-yoga
+
+## Reference
+
+- https://www.howtographql.com/graphql-js/2-a-simple-query/
+
+## How to use
+
+- http://localhost:4000
+
+```
+mutation {
+post(
+url: "www.prisma.io"
+description: "Prisma replaces traditional ORMs"
+) {
+id
+}
+}
+```
---
+++ 04-graphql-yoga/server.js
@@ -0,0 +1,34 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+let idCount = links.length;
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
