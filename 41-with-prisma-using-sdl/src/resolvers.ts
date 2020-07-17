e52357f9a275e14f4e7d27e3d65b83f2ff8da634
mingyuchoo <mingyuchoo@gmail.com>
2020-07-17 22:34:31 +0900

    modified

--- 41-with-prisma-using-sdl/src/resolvers.ts
+++ 41-with-prisma-using-sdl/src/resolvers.ts
@@ -4,26 +4,88 @@ import { Context } from './context';
 
 const resolvers: IResolvers = {
   Query: {
-    feed: (parent, args, ctx: Context) => {
-      return ctx.prisma.post.findMany({
-        where: { published: true },
+    getAllPosts: (parent, args, context: Context) => {
+      return context.prisma.post.findMany({});
+    },
+    getOnePostById: (parent, args, context: Context) => {
+      return context.prisma.post.findOne({
+        where: { id: Number(args.id) },
+      });
+    },
+    getAllUsers: (parent, args, context: Context) => {
+      return context.prisma.user.findMany({});
+    },
+    getOneUserById: (parent, args, context: Context) => {
+      return context.prisma.user.findOne({
+        where: { id: Number(args.id) },
+      });
+    },
+  },
+  Mutation: {
+    createPostByEmail: (parent, args, context: Context) => {
+      return context.prisma.post.create({
+        data: {
+          title: String(args.title),
+          content: String(args.content),
+          published: false,
+          author: {
+            connect: { email: String(args.email) },
+          },
+        },
+      });
+    },
+    updatePostById: (parent, args, context: Context) => {
+      return context.prisma.post.update({
+        data: {
+          title: String(args.title),
+          content: String(args.content),
+          published: Boolean(args.published),
+        },
+        where: { id: Number(args.id) },
+      });
+    },
+    deletePostById: (parent, args, context: Context) => {
+      return context.prisma.post.delete({
+        where: { id: Number(args.id) },
+      });
+    },
+    createUserByEmail: (parent, args, context: Context) => {
+      return context.prisma.user.create({
+        data: {
+          email: String(args.email),
+          name: String(args.name),
+        },
+      });
+    },
+    updateUserById: (parent, args, context: Context) => {
+      return context.prisma.user.update({
+        data: {
+          email: String(args.email),
+          name: String(args.name),
+        },
+        where: { id: Number(args.id) },
+      });
+    },
+    deleteUserById: (parent, args, context: Context) => {
+      return context.prisma.user.delete({
+        where: { id: Number(args.id) },
       });
     },
   },
   User: {
-    posts: (parent, args, ctx: Context) => {
-      return ctx.prisma.user
+    posts: (parent, args, context: Context) => {
+      return context.prisma.user
         .findOne({
-          where: { id: parent.id },
+          where: { id: Number(parent.id) },
         })
         .posts();
     },
   },
   Post: {
-    author: (parent, args, ctx: Context) => {
-      return ctx.prisma.post
+    author: (parent, args, context: Context) => {
+      return context.prisma.post
         .findOne({
-          where: { id: parent.id },
+          where: { id: Number(parent.id) },
         })
         .author();
     },
--- 41-with-prisma-using-sdl/src/schema.ts
+++ 41-with-prisma-using-sdl/src/schema.ts
@@ -2,7 +2,7 @@ import 'graphql-import-node';
 import { makeExecutableSchema } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
 
-import * as typeDefs from './schema/schema.graphql';
+import * as typeDefs from './schema/scheme.graphql';
 import resolvers from './resolvers';
 
 const schema: GraphQLSchema = makeExecutableSchema({
