0ca7ddf1641af7dd911921de177b2d434b46a035
mingyuchoo <mingyuchoo@gmail.com>
2020-09-17 06:01:21 +0900

    make it better

--- 42-apollo-express-with-prisma-using-sdl-splitted/src/schema.ts
+++ 42-apollo-express-with-prisma-using-sdl-splitted/src/schema.ts
@@ -3,7 +3,7 @@ import 'graphql-import-node';
 import { makeExecutableSchema } from 'graphql-tools';
 import { GraphQLSchema } from 'graphql';
 
-import * as typeDefs from './graphql/scheme.graphql';
+import * as typeDefs from './typeDefs/scheme.graphql';
 import * as resolvers from './resolvers';
 
 const schema: GraphQLSchema = makeExecutableSchema({
