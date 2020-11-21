b461e6f899343c672e3d58ea7811155e57f71d7d
mingyuchoo <mingyuchoo@gmail.com>
2020-11-21 15:32:51 +0900

    initial

--- 45-using-postgres-prisma-raw-sql/src/graphql/resolvers/index.ts
+++
@@ -1,6 +0,0 @@
-import { loadFilesSync, mergeResolvers } from 'graphql-tools';
-import path from 'path';
-
-const resolverArray = loadFilesSync(path.join(__dirname, './**/*.resolver.{ts,js}'), { recursive: true });
-
-export default mergeResolvers(resolverArray);
