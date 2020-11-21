17f9f0ced29f314fa2c5d90a4d0badb58f71a02c
mingyuchoo <mingyuchoo@gmail.com>
2020-11-21 14:27:07 +0900

    initial

--- 30-using-sequelize-sqlite/seeders/create_tables.sh
+++
@@ -1,2 +0,0 @@
-node_modules/.bin/sequelize model:create --name User --attributes name:string,email:string,password:string
-node_modules/.bin/sequelize model:create --name Recipe --attributes title:string,ingredients:text,direction:text
