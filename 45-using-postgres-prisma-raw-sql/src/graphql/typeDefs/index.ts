1d3cd7d5a04cb5fcf0b9ebb02fcb2b1ddec9c866
mingyuchoo <mingyuchoo@gmail.com>
2020-11-21 11:26:05 +0900

    initial

--- 45-using-postgres-prisma-raw-sql/src/graphql/typeDefs/index.ts
+++
@@ -1,6 +0,0 @@
-import { loadFilesSync, mergeTypeDefs } from 'graphql-tools';
-import path from 'path';
-
-const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'), { recursive: true });
-
-export default mergeTypeDefs(typesArray);
